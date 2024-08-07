b2e3513427e744632799e982082b0a388afc8e79
BogdanKolesnikow <kolesnikowbog@gmail.com>
2024-08-07 16:04:16 +0700

    Update les8_HM.cpp

--- les8_HM/les8_HM/les8_HM.cpp
+++ les8_HM/les8_HM/les8_HM.cpp
@@ -8,7 +8,13 @@ using namespace std;
 void main()
 {
     setlocale(LC_ALL, "Rus");
-    int num0 = 12, num1 = 5, num2 = 87;
+    int num0 = 0, num1 = 0, num2 = 0;
+    cout << "Введите число 1 ";
+    cin >> num0;
+    cout << "Введите число 1 ";
+    cin >> num1;
+    cout << "Введите число 1 ";
+    cin >> num2;
     int sum0 = 0, sum1 = 0, sum2 = 0, sum4 = (num0+num1+num2)/3;
     cout << "\nСумма 3х чисел:_ " << num0 + num1 + num2 << "\nСреднее арифметическое 3х чисел:_ " << sum4 << "\nпроизведение 3х чисел:_ " << num0 * num1 * num2;
 }
