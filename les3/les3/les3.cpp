548f40681c962ae81ba82d36c2883b407cdb52d4
BogdanKolesnikow <kolesnikowbog@gmail.com>
2024-08-07 09:46:11 +0700

    Add files via upload

---
+++ les3/les3/les3.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+using namespace std;
+
+/*
+int main()
+{
+    setlocale(LC_ALL, "Rus");
+    std::cout << "Hello World!\n";
+}
+*/
+
+
+int main()
+{
+setlocale(LC_ALL, "Rus");
+    std::cout << "\t\t\t\t\tЛюбви, надежды, тихой славы\n";
+    std::cout << "\t\t\t\t\tНедолго нежил нас обман\n";
+}
+
+/*P.S А.С.Пушкин*/
