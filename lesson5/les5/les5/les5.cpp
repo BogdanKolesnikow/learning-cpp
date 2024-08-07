d4b7f3c31e894ff9f7160f18478782882849f800
BogdanKolesnikow <kolesnikowbog@gmail.com>
2024-08-07 14:37:22 +0700

    Add files via upload

---
+++ lesson5/les5/les5/les5.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+using namespace std;
+
+void main()
+{
+    setlocale(LC_ALL, "Rus");
+    int a = 0, b = 2, v = 3;
+    cout << a <<  " " <<  b << " " << v; "/n";
+}
+
+
+/*
+cout <<  << endl;
+*/
