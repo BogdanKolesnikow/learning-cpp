9759669d90dc77483ee6f9de406148b8cf8a4f0c
BogdanKolesnikow <kolesnikowbog@gmail.com>
2024-08-07 15:08:23 +0700

    Add files via upload

---
+++ les7/les7/les7.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+using namespace std;
+
+void main()
+{
+    setlocale(LC_ALL, "Rus");
+    int var = 0, var2 = 0;
+    cout << "Введите число 1 ";
+    cin >> var;
+    cout << "Введите число 2 ";
+    cin >> var;
+    cout << "вы ввели 2 гавна : \n" << var << " <--1\n" << var2 << " <--2\n"; "\n";
+}
+
+
+/*
+cout << _; "/n";
+*/
