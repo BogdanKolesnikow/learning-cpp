39f35b488592280fe23f9371127174040e32d5cf
BogdanKolesnikow <kolesnikowbog@gmail.com>
2024-08-07 17:13:44 +0700

    Add files via upload

---
+++ les11/les11/les11.cpp
@@ -0,0 +1,13 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int a = 2, b = 1488;
+	cout << ((a == b) && (5 > 4)); "\n"; 
+	cout << ((a == b) || (0.25436734222 < 4)); "\n";
+}
+
+/*
+cout << "\n";
+*/
