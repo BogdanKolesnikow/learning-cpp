d82039bdb1535b1d4eaf68ea4d560c6f937446bb
BogdanKolesnikow <kolesnikowbog@gmail.com>
2024-08-07 14:50:26 +0700

    Add files via upload

---
+++ les6/les6/les6.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+using namespace std;
+
+void main()
+{
+    setlocale(LC_ALL, "Rus");
+    const int DW = 0;
+    const char NL = '\n';
+    const char Tb = '\t';
+    cout << DW << NL << Tb << Tb;
+}
+
+
+/*
+cout << _; "/n";
+*/
