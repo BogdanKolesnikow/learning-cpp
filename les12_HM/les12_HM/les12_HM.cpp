53138a40efbc10ee927cc4e0651b0fb7c5084725
BogdanKolesnikow <kolesnikowbog@gmail.com>
2024-08-07 17:57:43 +0700

    Add files via upload

---
+++ les12_HM/les12_HM/les12_HM.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+using namespace std;
+
+void main() {
+    setlocale(LC_ALL, "ru");
+    int num;
+    cout << ("Введите число:");
+    cin >> num;
+    if (num % 2 == 0)
+    {
+        cout << ("Это чётное число \n");
+    }
+    else
+    {
+        cout << ("Это Нечётное число \n");
+    }
+}
