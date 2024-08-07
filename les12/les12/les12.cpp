67f70ff3d012614793030cd99a3dbea987b6759c
BogdanKolesnikow <kolesnikowbog@gmail.com>
2024-08-07 17:52:18 +0700

    Add files via upload

---
+++ les12/les12/les12.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "ru");
+	int num;
+	cout << "введи число:_ \n";
+	cin >> num;
+	if (num > 5)
+	{
+		cout << "число больше 5ти\n";
+	}
+	else if(num==5)
+	{
+		cout << "число равно 5ти\n";
+	}
+	else
+	{
+		cout << "число меньше 5ти\n";
+	}
+
+}
+
+
+/*
+cout << "\n";
+*/
