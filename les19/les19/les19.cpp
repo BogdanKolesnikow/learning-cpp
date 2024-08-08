c496200879383acbb507ef069fae436f916fccc3
BogdanKolesnikow <kolesnikowbog@gmail.com>
2024-08-08 21:00:19 +0700

    Add files via upload

---
+++ les19/les19/les19.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "ru");
+	for (int i = 0; i < 30; i++)
+	{
+		if (i==10) 
+		{
+			continue;
+		}
+		cout << "\nеременная i = " << i; '\n';
+	}
+}
+
+/*
+cout << "\n";
+*/
