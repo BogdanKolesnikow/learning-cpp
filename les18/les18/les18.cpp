29867501332bec184d36c37d62d9f60b35781928
BogdanKolesnikow <kolesnikowbog@gmail.com>
2024-08-08 20:39:58 +0700

    Add files via upload

---
+++ les18/les18/les18.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "ru");
+
+	for (int i = 0; ; i++)
+	{
+		cout << "__" << i << "# __\n";
+		if (i==15)
+		{
+			break;
+		}
+	}
+	cout << "конец цикла\n";
+}
+
+/*
+cout << "\n";
+*/
