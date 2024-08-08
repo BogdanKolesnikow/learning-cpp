1e85a184c98f6675bac37c6b98296db66a679fd6
BogdanKolesnikow <kolesnikowbog@gmail.com>
2024-08-08 15:26:28 +0700

    Add files via upload

---
+++ les13/les13/les13.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "ru");
+	int a;
+	cin >> a;
+
+	switch (a)
+	{
+	case 1:
+		cout << "вы ввели 1\n";
+		break;
+	case 2:
+		cout << "вы ввели 2\n";
+		break;
+	default:
+		cout << "вы ввели не то число\n";
+		break;
+	}
+}
+
+
+/*
+cout << "\n";
+*/
