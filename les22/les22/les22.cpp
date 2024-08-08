0470298c2f3dc9815e415437428bb22bb119c0c3
BogdanKolesnikow <kolesnikowbog@gmail.com>
2024-08-08 21:09:18 +0700

    Add files via upload

---
+++ les22/les22/les22.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "ru");
+	
+	cout << "1\n";
+	goto link;
+	cout << "2\n";
+	cout << "3\n";
+	link:
+	cout << "4\n";
+	cout << "5\n";
+
+	system("pause");
+}
+
+/*
+cout << "\n";
+*/
