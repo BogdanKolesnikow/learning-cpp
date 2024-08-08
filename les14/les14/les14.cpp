62b8f029412022ace2ae53e07cd810b2fe08358f
BogdanKolesnikow <kolesnikowbog@gmail.com>
2024-08-08 16:06:12 +0700

    Add files via upload

---
+++ les14/les14/les14.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "ru");
+	
+	int a = 0;
+
+	while (a < 10)
+	{
+		cout << "текст\n" << a; '\n';
+		a++;
+	}
+}
+
+/*
+cout << "\n";
+*/
