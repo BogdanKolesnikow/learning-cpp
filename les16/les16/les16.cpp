aa2730e819aa5c6b6202cca3f7e289f67683e8c8
BogdanKolesnikow <kolesnikowbog@gmail.com>
2024-08-08 19:48:01 +0700

    Add files via upload

---
+++ les16/les16/les16.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "ru");
+
+	for (int i = 0; i < 10; i++)
+	{
+		cout << "топ причин почему я учу С++: " << i << "# конечно сделать ту ду лист\n";
+	}
+}
+
+/*
+cout << "\n";
+*/
