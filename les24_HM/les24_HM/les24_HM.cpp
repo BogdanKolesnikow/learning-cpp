eb66af94e8a77fb0470dc7b917eaa40cced4b6e0
BogdanKolesnikow <kolesnikowbog@gmail.com>
2024-08-09 10:12:33 +0700

    Add files via upload

---
+++ les24_HM/les24_HM/les24_HM.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "ru");
+	int len = '*', wid = '*';
+	cout << "какая длинна?\n";
+	cin >> len;
+	cout << "какая ширина?\n";
+	cin >> wid;
+	for (int j = 0; j < wid; j++)
+	{
+		for (int i = 0; i < len; i++)
+		{
+			cout << "*";
+		}
+		cout << "\n";
+	}
+	cout << "\n";
+}
+
+/*
+cout << "\n";
+
+--->>
+*****
+*****
+*****
+ ширина 5 высота 3
+*/
