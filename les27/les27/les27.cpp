631cd7907c3efa1235a024c8e63f72d6fbc1dfb6
BogdanKolesnikow <kolesnikowbog@gmail.com>
2024-08-09 11:20:12 +0700

    Add files via upload

---
+++ les27/les27/les27.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "ru");
+
+	const  int sz = 7;
+	int ar0['sz']{1,223342,4678,-4354543, 47, 57983,7483};
+	for (int i = 0; i < sz; i++)
+	{
+		cout << ar0[i] << '\n';
+	}
+}
+
+/*
+cout << "\n";
+*/
