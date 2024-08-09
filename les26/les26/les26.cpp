b9bd3f14136f6efb70b874c21a5aeff975a1dbd7
BogdanKolesnikow <kolesnikowbog@gmail.com>
2024-08-09 11:07:12 +0700

    Add files via upload

---
+++ les26/les26/les26.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "ru");
+
+	int ar0[]{1,223342,4678,-4354543, 47, 57983,7483};
+	cout << ar0[3] << "\n";
+}
+
+/*
+cout << "\n";
+*/
